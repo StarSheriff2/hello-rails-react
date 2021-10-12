a9a4777ea82dbd873aafa6bed018d8b590f24fb5
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-11 18:31:37 -0600

    Greetings#Index test works

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,4 +1,15 @@
 class V1::GreetingsController < ApplicationController
+  def index
+    render json: { :greetings => [
+      {
+        message: 'Whats up'
+      },
+      {
+        message: 'Hello there'
+      }
+    ] }.to_json
+  end
   def hello
+
   end
 end
