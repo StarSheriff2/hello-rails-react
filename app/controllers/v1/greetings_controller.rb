afa9cfb58bbebd54b686c58ad7b9c331ddf8994f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-12 10:50:06 -0600

    Remove Greetings#index action

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,15 +1,4 @@
 class V1::GreetingsController < ApplicationController
-  def index
-    render json: { greetings: [
-      {
-        message: 'Whats up'
-      },
-      {
-        message: 'Hello there'
-      }
-    ] }.to_json
-  end
-
   def hello
     @greeting = Greeting.find(Greeting.pluck(:id).sample)
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
-    get 'greetings', to: 'greetings#index'
     get 'random-greeting', to: 'greetings#hello'
   end
 
