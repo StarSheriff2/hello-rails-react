76141049735a667b72a211fdc8c6f70de7461303
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-12 11:34:26 -0600

    Fix linter errors

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,22 +1,24 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import PropTypes from 'prop-types';
 import { fetchGreeting } from '../redux/greeting/greeting';
 
 const Greeting = () => {
   const dispatch = useDispatch();
 
-  const greeting = useSelector((state) => state.greeting.message );
+  const greeting = useSelector((state) => state.greeting.message);
 
   useEffect(() => {
     dispatch(fetchGreeting());
-  }, [])
+  }, []);
 
   return (
     <>
-      <h1>Greeting: {greeting}</h1>
+      <h1>
+        Greeting:
+        {greeting}
+      </h1>
     </>
   );
-}
+};
 
 export default Greeting;
